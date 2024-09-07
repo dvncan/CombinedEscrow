d0e3dad8c66f4f0c3f26d774b9f04328b4a30c5a
dvncan <dunbrown64@gmail.com>
2024-09-06 23:53:54 -0400

    improved reentrancy

--- test/AdvancedCombinedEscrowTests.t.sol
+++ test/AdvancedCombinedEscrowTests.t.sol
@@ -30,13 +30,21 @@ contract AdvancedCombinedEscrowTests is
         vm.expectRevert("EscrowStateError()");
         escrow.deposit{value: 1 ether}(user);
     }
+
+    /********************************************************************************
+     *                                                                              *
+     *           Test multiple deposits from different users and single withdrawal  *
+     *           █████████████████████████████████████████████████████████████████  *
+     *           █   1. Deposit from user1                                          *
+     *           █   2. Deposit from user2                                          *
+     *           █   3. Refund the escrow                                           *
+     *           █   4. Withdraw twice to a single address                          *
+     *           █   5. Assert correct balances                                     *
+     *           █████████████████████████████████                                  *
+     *                                                                              *
+     ********************************************************************************/
+
     function test_multipleDeposits_singleWithdraw_steal() public {
-        // Test multiple deposits from different users and single withdrawal
-        // 1. Deposit from user1
-        // 2. Deposit from user2
-        // 3. Refund the escrow
-        // 4. Withdraw all funds to a single address
-        // 5. Assert correct balances
         vm.deal(user, 1 ether);
         (address alice, uint256 alicePk) = makeAddrAndKey("alice");
         vm.deal(alice, 1 ether);
@@ -50,15 +58,24 @@ contract AdvancedCombinedEscrowTests is
         escrow.withdraw(payable(user));
         vm.expectRevert(InputValidationError.selector);
         escrow.withdraw(payable(user));
+        assertEq(user.balance, 1 ether, "Fail");
+        assertEq(alice.balance, 0, "Fail");
     }
 
+    /********************************************************************************
+     *                                                                              *
+     *           Test multiple deposits from different users and single withdrawal  *
+     *           █████████████████████████████████████████████████████████████████  *
+     *           █   1. Deposit from user1                                          *
+     *           █   2. Deposit from user2                                          *
+     *           █   3. Refund the escrow                                           *
+     *           █   4. Withdraw all funds to a single address                      *
+     *           █   5. Assert correct balances                                     *
+     *           █████████████████████████████████                                  *
+     *                                                                              *
+     ********************************************************************************/
+
     function test_multipleDeposits_singleWithdraw() public {
-        // Test multiple deposits from different users and single withdrawal
-        // 1. Deposit from user1
-        // 2. Deposit from user2
-        // 3. Refund the escrow
-        // 4. Withdraw all funds to a single address
-        // 5. Assert correct balances
         vm.deal(user, 1 ether);
         // (address alice, uint256 alicePk) = makeAddrAndKey("alice");
         // vm.deal(alice, 1 ether);
@@ -76,21 +93,80 @@ contract AdvancedCombinedEscrowTests is
         assertEq(user.balance, 1 ether, "user.balance - Fail");
     }
 
+    /********************************************************************************
+     *                                                                              *
+     *           Test multiple deposits from different users and single withdrawal  *
+     *           █████████████████████████████████████████████████████████████████  *
+     *           █   1. Deposit from user1                                          *
+     *           █   2. Deposit from user1                                          *
+     *           █   3. Refund the escrow                                           *
+     *           █   4. Withdraw all funds to a single address                      *
+     *           █   5. Assert correct balances                                     *
+     *           █████████████████████████████████                                  *
+     *                                                                              *
+     ********************************************************************************/
+
     function test_multipleDeposits_singleWithdraw_project() public {
-        // Test multiple deposits from different users and single withdrawal
         // 1. Deposit from user1
         // 2. Deposit from user1
         // 3. Refund the escrow
-        // 4. user2 attempts to steal user1
+        // 4.
         // 5. Withdraw all funds to a single address < attempt to steal >
         // 6. Assert correct balances
     }
 
+    function testReentrancyProtectionStrict() public {
+        // Create a malicious contract that tries to re-enter the withdraw function
+        MaliciousContract maliciousContract = new MaliciousContract(
+            address(escrow)
+        );
+
+        // Additional test logic
+        (address ai, uint256 aiPk) = makeAddrAndKey("ai");
+        vm.prank(ai);
+        vm.deal(ai, 1 ether);
+        maliciousContract.deposit{value: 1 ether}();
+        // escrow.deposit{value: 1 ether}(ai);
+
+        // Deposit funds
+        uint256 depositAmount = 1 ether;
+        vm.deal(address(maliciousContract), depositAmount);
+        vm.prank(address(maliciousContract));
+        maliciousContract.deposit{value: depositAmount}();
+
+        // Refund the escrow
+        user_refund_escrow(escrow, _owner);
+
+        // Attempt to withdraw using the malicious contract
+        // vm.expectRevert("revert: ReentrancyGuard: reentrant call");
+        vm.expectRevert(); // "revert: ReentrancyGuard: reentrant call"
+        vm.prank(address(maliciousContract));
+        escrow.withdraw(payable(address(maliciousContract)));
+
+        // Assert that the reentrancy attack failed
+        assertEq(
+            address(escrow).balance,
+            2 * depositAmount,
+            "Escrow balance should remain unchanged"
+        );
+        assertEq(
+            address(maliciousContract).balance,
+            0,
+            "Malicious contract should not receive funds"
+        );
+    }
     function testReentrancyProtection() public {
         // Create a malicious contract that tries to re-enter the withdraw function
         MaliciousContract maliciousContract = new MaliciousContract(
             address(escrow)
         );
+
+        // Additional test logic
+        // (address ai, uint256 aiPk) = makeAddrAndKey("ai");
+        // vm.prank(ai);
+        // vm.deal(ai, 1 ether);
+        // // maliciousContract.deposit{value: 1 ether}();
+        // escrow.deposit{value: 1 ether}(ai);
 
         // Deposit funds
         uint256 depositAmount = 1 ether;
